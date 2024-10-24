307a2fdb11910649ca43af3619d6b56fd4442b97
hyeondong-hong <h2dong@kakao.com>
2024-10-25 05:43:45 +0900

    add: 동시성 테스트 추가 및 SQL 파일 추가
    테스트 환경 구축을 위해 테이블에 샘플 데이터를 삽입하는 SQL 파일을 추가했습니다. 이를 통해 테스트 시 즉시 완성된 데이터를 활용할 수 있습니다.

---
+++ src/test/resources/sql/concert_schedule.sql
@@ -0,0 +1,6 @@
+INSERT INTO hhplus_concert_test.concert_schedule (id, concert_id, scheduled_at) VALUES (1, 1, '2024-10-18 21:00:00.000000');
+INSERT INTO hhplus_concert_test.concert_schedule (id, concert_id, scheduled_at) VALUES (2, 1, '2024-10-18 22:00:00.000000');
+INSERT INTO hhplus_concert_test.concert_schedule (id, concert_id, scheduled_at) VALUES (3, 1, '2024-10-18 23:00:00.000000');
+INSERT INTO hhplus_concert_test.concert_schedule (id, concert_id, scheduled_at) VALUES (4, 1, '2024-10-19 21:00:00.000000');
+INSERT INTO hhplus_concert_test.concert_schedule (id, concert_id, scheduled_at) VALUES (5, 1, '2024-10-19 22:00:00.000000');
+INSERT INTO hhplus_concert_test.concert_schedule (id, concert_id, scheduled_at) VALUES (6, 1, '2024-10-19 23:00:00.000000');
