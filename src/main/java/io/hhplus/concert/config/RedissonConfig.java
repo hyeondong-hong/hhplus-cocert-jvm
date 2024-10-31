fa69a2785a348935e24ec740aeb8c7e092b921a8
hyeondong-hong <h2dong@kakao.com>
2024-11-01 01:06:51 +0900

    feat: Redis 설정 추가
    Redis를 사용하기 위해 Redisson 설정 및 패키지 의존성을 추가했습니다.

---
+++ src/main/java/io/hhplus/concert/config/RedissonConfig.java
@@ -0,0 +1,39 @@
+package io.hhplus.concert.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${redisson.address}")
+    private String address;
+
+    @Value("${redisson.password}")
+    private String password;
+
+    @Value("${redisson.timeout}")
+    private Integer timeout;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        SingleServerConfig ssc = config.useSingleServer();
+        ssc.setAddress(address);
+        if (password != null && !password.isEmpty()) {
+            ssc.setPassword(password);
+        }
+        if (timeout != null && timeout > 0) {
+            ssc.setTimeout(timeout);
+        }
+
+        return Redisson.create(config);
+    }
+}
